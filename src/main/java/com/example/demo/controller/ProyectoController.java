2690185c595a09c7bcdcb176bea8681144bfadf8
JouseJG <jibuza@riseup.net>
2026-09-22 17:01:04 +0200

    Completar CRUD en memoria

--- src/main/java/com/example/demo/controller/ProyectoController.java
+++ src/main/java/com/example/demo/controller/ProyectoController.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import com.example.demo.model.Proyecto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,19 +19,21 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class ProyectoController {
 
     private final List<Proyecto> proyectos = new ArrayList<>();
+    private int siguienteId = 1;
 
     @GetMapping
-    public List<Proyecto> lista(@RequestParam(name = "estado", defaultValue = "todos") String estado) {
-        if(estado.equals("activo")){
-            List<Proyecto> filtrado = new ArrayList<>();
-            for (Proyecto proyecto : proyectos) {
-                if (proyecto.getActivo()) {
-                    filtrado.add(proyecto);
-                }
-            }
-            return filtrado;
+    public List<Proyecto> lista(@RequestParam(name = "activo", required = false) Boolean activo) {
+        if(activo == null){
+            return  proyectos;
         }
-        return proyectos;
+
+        List<Proyecto> filtrado = new ArrayList<>(); 
+        for (Proyecto proyecto : proyectos) {
+            if (activo == proyecto.getActivo()) {
+                filtrado.add(proyecto);
+            }
+        }
+        return filtrado;
     }
 
     @GetMapping("/{id}")
@@ -49,8 +53,30 @@ public class ProyectoController {
 
     @PostMapping
     public Proyecto crear(@RequestBody Proyecto proyecto) {
+        proyecto.setId(siguienteId);
+        siguienteId += 1;
         proyectos.add(proyecto);
         return proyecto;
+    }
+
+    @PutMapping("/{id}")
+    public Proyecto actualizar(
+        @PathVariable(name = "id") int id,
+        @RequestBody Proyecto datos) {
+            
+        for (int i = 0; i < proyectos.size(); i++) {
+            if (proyectos.get(i).getId() == id) {
+                datos.setId(id);
+                proyectos.set(i, datos);
+                return datos;
+            }
+        }
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    public void eliminar(@PathVariable(name = "id") int id) {
+        proyectos.removeIf(proyecto -> proyecto.getId() == id);
     }
 }
 
